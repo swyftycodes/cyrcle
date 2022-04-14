718ef0ff0cd9d694d60fd968246ff4105cc01289
Jensen Lai <ninnu.0123p@gmail.com>
2022-04-14 10:36:05 +1000

    Bal command complete

--- commands/bal.js
+++ commands/bal.js
@@ -2,5 +2,12 @@ module.exports = async (message, users, initAcc) => {
     await initAcc(message.author.id)
     
     userDetails = await users.findOne({id: message.author.id})
-    console.log(userDetails.coins)
+
+    const em = {
+        color: '#fde65e',
+        title: `${message.author.tag}'s balance'`,
+        description: `wallet: ${userDetails.coins}`
+    }
+
+    message.reply({ embeds: [em] });
 }
